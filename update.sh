dfba4f5fe71abf6f738e3b6a8ceea107948d1258
Patrick Darwinkel <patrick@darwinkel.net>
2024-02-26 20:59:34 +0100

    Fix docker compose order

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+docker compose pull
+
 CONTAINER_IMAGE_ID=$(docker compose images -q  | awk '{print "sha256:"$1}')
 LATEST_IMAGE_ID=$(docker inspect --format='{{.Id}}' ghcr.io/darwinkel/shepardtts:main)
 
 if [ "$LATEST_IMAGE_ID" != "$CONTAINER_IMAGE_ID" ]
 then
-    docker compose pull
     docker compose stop
     docker compose down
     docker compose up -d
