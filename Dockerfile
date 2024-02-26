5b7ace34e76677997b1f3b198b86377165b555e0
Patrick Darwinkel <patrick@darwinkel.net>
2024-02-26 20:39:48 +0100

    Update script; attempt to use slim image (may not work)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11-bookworm
+FROM python:3.11-slim-bookworm
 
 WORKDIR /usr/src/app
 
--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if ! docker pull ghcr.io/darwinkel/shepardtts:main | grep "Image is up to date for"
+
+CONTAINER_IMAGE_ID=$(docker compose images -q  | awk '{print "sha256:"$1}')
+LATEST_IMAGE_ID=$(docker inspect --format='{{.Id}}' ghcr.io/darwinkel/shepardtts:main)
+
+if [ "$LATEST_IMAGE_ID" != "$CONTAINER_IMAGE_ID" ]
 then
+    docker compose pull
     docker compose stop
     docker compose down
     docker compose up -d
